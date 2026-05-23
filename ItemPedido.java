11cff7afff629759a8a73ab74cb4b8d617abca76
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-05-23 13:39:37 -0300

    Execoes terminadas para que nao aja erro na hora de colocar os valores na area pedido

--- ItemPedido.java
+++ ItemPedido.java
@@ -10,5 +10,36 @@ public class ItemPedido {
         this.setQuantidade(quantidade);
         this.setPrecoUnitario(precoUnitario);
     }
+    
+    public int getId() { return this.id; }
+    
+    public void setId(int id) { 
+        if (id >= 0) {
+            this.id = id; 
+        }
+    }
 
+    public int getIdProduto() { return this.idProduto; }
+    
+    public void setIdProduto(int idProduto) { 
+        if (idProduto > 0) {
+            this.idProduto = idProduto; 
+        }
+    }
+
+    public int getQuantidade() { return this.quantidadeVolume; }
+    
+    public void setQuantidade(int quantidade) { 
+        if (quantidade > 0) {
+            this.quantidadeVolume = quantidade; 
+        }
+    }
+
+    public double getPrecoUnitario() { return this.precoUnitario; }
+    
+    public void setPrecoUnitario(double precoUnitario) { 
+        if (precoUnitario > 0) {
+            this.precoUnitario = precoUnitario; 
+        }
+    }
 }
