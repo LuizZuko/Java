00d1312e9fe3b764a840c303e4f753b8f5af7a9b
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-05-24 01:12:06 -0300

    adiciona classe StatusPedido com constantes numéricas

---
+++ StatusPedido.java
@@ -0,0 +1,7 @@
+public final class StatusPedido {
+    public static final int FILA = 1;
+    public static final int PROCESSANDO = 2;
+    public static final int FINALIZADO = 3;
+
+    private StatusPedido() {}
+}
