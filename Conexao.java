3b9269b919bdfed9d0d737ff119ec9f96ca36629
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-05-25 12:29:41 -0300

    Ligação do mysql com o java pelo jdbc

---
+++ Conexao.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/ervamate_industrial_db";
+    private static final String USER = "root";
+    private static final String PASS = "senha";
+
+    public static Connection getConexao() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
