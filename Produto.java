db40eb931c1b14cf775cc4f294dfb2b06c3ad4c1
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-05-26 12:17:59 -0300

    adiciona o construtor. Ele serve para obrigar o sistema a passar os dados do produto na hora da criação, direcionando esses dados para os métodos de validação.

--- Produto.java
+++ Produto.java
@@ -5,6 +5,14 @@ public class Produto {
     private int estoque;
     private int categoria;
 
+    public Produto(int id, String nome, double preco, int estoque, int categoria) {
+        this.setId(id);
+        this.setNome(nome);
+        this.setPreco(preco);
+        this.setEstoque(estoque);
+        this.setCategoria(categoria);
+    }
+
     public int getId() { return this.id; }
     public String getNome() { return this.nome; }
     public double getPreco() { return this.preco; }
