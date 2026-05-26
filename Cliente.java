72a47e23b8e2b7e35b01d8f82eb3b1a9bcc0df00
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-05-26 20:48:43 -0300

    criando a estrutura inicial da classe Cliente e seu construtor

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+
+    public Cliente(int id, String nome, String email) {
+        this.setId(id);
+        this.setNome(nome);
+        this.setEmail(email);
+    }
+}
