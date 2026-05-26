130f1dfd5ab7ccaeb0580a5b612778e4a8ed6fb3
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-05-26 11:54:44 -0300

    adiciona metodos getters para leitura dos atributos de Produto

--- Produto.java
+++ Produto.java
@@ -4,4 +4,10 @@ public class Produto {
     private double preco;
     private int estoque;
     private int categoria;
+
+    public int getId() { return this.id; }
+    public String getNome() { return this.nome; }
+    public double getPreco() { return this.preco; }
+    public int getEstoque() { return this.estoque; }
+    public int getCategoria() { return this.categoria; }
 }
