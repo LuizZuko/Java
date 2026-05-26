f21d49fb320f7029554f09be7859b370f1892b1a
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-05-26 20:59:31 -0300

    adiciona getters e setters com validações para Id, Nome e Email

--- Cliente.java
+++ Cliente.java
@@ -8,4 +8,27 @@ public class Cliente {
         this.setNome(nome);
         this.setEmail(email);
     }
+public int getId() { return this.id; }
+    
+    public void setId(int id) { 
+        if (id >= 0) {
+            this.id = id; 
+        }
+    }
+
+    public String getNome() { return this.nome; }
+    
+    public void setNome(String nome) { 
+        if (nome != null && nome.trim().length() > 0) {
+            this.nome = nome.trim(); 
+        }
+    }
+
+    public String getEmail() { return this.email; }
+    
+    public void setEmail(String email) { 
+        if (email != null && email.trim().length() > 0) {
+            this.email = email.trim(); 
+        }
+    }
 }
