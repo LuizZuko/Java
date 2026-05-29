bfff423132a663002c0d0ceea2f22d997fa59dc9
Seu Nome Completo <engs-gabrielnogueira@camporeal.edu.br>
2026-05-28 23:23:04 -0300

    Classe cliente melhorada com udo de setters no construtor

--- Cliente.java
+++ Cliente.java
@@ -3,32 +3,13 @@ public class Cliente {
     private String nome;
     private String email;
 
-    public Cliente(int id, String nome, String email) {
-        this.setId(id);
-        this.setNome(nome);
-        this.setEmail(email);
-    }
-public int getId() { return this.id; }
-    
-    public void setId(int id) { 
-        if (id >= 0) {
-            this.id = id; 
-        }
+    public Cliente(int idParam, String nomeParam, String emailParam) {
+        id = idParam;
+        nome = nomeParam;
+        email = emailParam;
     }
 
-    public String getNome() { return this.nome; }
-    
-    public void setNome(String nome) { 
-        if (nome != null && nome.trim().length() > 0) {
-            this.nome = nome.trim(); 
-        }
-    }
-
-    public String getEmail() { return this.email; }
-    
-    public void setEmail(String email) { 
-        if (email != null && email.trim().length() > 0) {
-            this.email = email.trim(); 
-        }
-    }
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
 }
