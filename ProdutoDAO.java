377507bc1637b5ccc25ae05a1f5f4776de4eb4e1
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-05-29 12:59:06 -0300

    insere produto de forma segura usando o metodo DAO

---
+++ ProdutoDAO.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    public static void inserir(String nome, double preco, int estoque, String categoria, String tipoErva) throws Exception {
+        String sql = "INSERT INTO produto (nome, preco, quantidade_estoque, categoria, tipo_erva) VALUES (?, ?, ?, ?, ?)";
+        
+        try (Connection conn = ConexaoFactory.getConexao();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, nome);
+            stmt.setDouble(2, preco);
+            stmt.setInt(3, estoque);
+            stmt.setString(4, categoria);
+            stmt.setString(5, tipoErva);
+            stmt.executeUpdate();
+        }
+    }
+}
