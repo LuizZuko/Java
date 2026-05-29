8b68605d92d5636ed8b59c7d4901c18f335b275a
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-05-29 13:00:17 -0300

    Escrevi o nome do jeito errado, ja concertei

--- ProdutoDAO.java
+++ ProdutoDAO.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class ProdutoDAO {
     public static void inserir(String nome, double preco, int estoque, String categoria, String tipoErva) throws Exception {
         String sql = "INSERT INTO produto (nome, preco, quantidade_estoque, categoria, tipo_erva) VALUES (?, ?, ?, ?, ?)";
-        
-        try (Connection conn = ConexaoFactory.getConexao();
+
+        try (Connection conn = Conexao.getConexao();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, nome);
             stmt.setDouble(2, preco);
